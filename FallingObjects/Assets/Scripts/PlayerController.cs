ddac472b956869c0a6f85c2550d87eb5eb026c93
charmseer <charmseer@gmail.com>
2021-05-04 18:22:51 +0530

    Create Player Controller Script
    Script was created and attached to player.
    Bounds are set on both sides to restrict player movement in that direction.

---
+++ FallingObjects/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 8.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Revieve input from keyboard
+        horizontalInput = Input.GetAxis("Horizontal");
+        //Code to make player move as per input
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-8, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(8, transform.position.y, transform.position.z);
+        }
+    }
+}
