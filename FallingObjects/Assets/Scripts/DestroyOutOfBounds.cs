330e7e7d0e34b0fa8e610fac5236898b9a4329d2
charmseer <charmseer@gmail.com>
2021-05-05 16:07:28 +0530

    Add script to destroy Collectibles and Projectile off screen
    DestroyOutOfBounds Script is created and added to projectiles and collectibles.
    
    Physics material added to Ground to decrease friction

---
+++ FallingObjects/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float lowerBound = 10.0f;
+    private float topBound = 10.0f;
+    // Start is calledbefore the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.z < -lowerBound)
+        {
+            Destroy(gameObject);
+        }
+
+        else if(transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
