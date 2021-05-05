2afb19855939cefd97b4b9ecadf6b80d27034740
charmseer <charmseer@gmail.com>
2021-05-05 16:38:29 +0530

    Configure Spawnmanager to randomize and spawn Object at regular intervals

--- FallingObjects/Assets/Scripts/SpawnManager.cs
+++ FallingObjects/Assets/Scripts/SpawnManager.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject[] FruitPrefabs;
+    public GameObject[] fruitPrefabs;
+    private float spawnRangeX = 7.5f;
+    private float spawnPosZ = 8.0f;
+    private float startDelay = 2.0f;
+    private float spawnInterval = 1.5f;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
+ 
         
+    }
+    void SpawnRandomAnimal()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        int fruitIndex = Random.Range(0, fruitPrefabs.Length);
+        Instantiate(fruitPrefabs[fruitIndex], spawnPos, fruitPrefabs[fruitIndex].transform.rotation);
     }
 }
