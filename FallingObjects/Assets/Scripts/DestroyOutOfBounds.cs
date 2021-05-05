5bb8ff64d04fd8356abfc96342155697a3e580fd
charmseer <charmseer@gmail.com>
2021-05-05 16:56:11 +0530

    Create Game Over test code

--- FallingObjects/Assets/Scripts/DestroyOutOfBounds.cs
+++ FallingObjects/Assets/Scripts/DestroyOutOfBounds.cs
@@ -17,6 +17,7 @@ public class DestroyOutOfBounds : MonoBehaviour
     {
         if(transform.position.z < -lowerBound)
         {
+            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
 
@@ -24,5 +25,6 @@ public class DestroyOutOfBounds : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
     }
 }
